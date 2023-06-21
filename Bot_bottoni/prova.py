b36dc232cb50de22e64683cf5f14a06eb11011cd
Smaldy <aymanhajali11@gmail.com>
2023-06-21 13:14:50 +0200

    Update prova.py

--- Bot_bottoni/prova.py
+++ Bot_bottoni/prova.py
@@ -1,13 +1,12 @@
-import csv
+import pandas as pd
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-bot = Bot(token='6265945877:AAGeiaPR9mmYXT_6xCGo-gN5fGGyUqBuXAI')
+bot = Bot(token='TOKEN')
 dp = Dispatcher(bot)
 button1 = KeyboardButton('Hello!')
 button2 = KeyboardButton('Youtube')
-button3 = KeyboardButton('prova')
-keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button1, button2,button3)
+keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button1, button2)
 
 @dp.message_handler()
 async def handle_messages(message: types.Message):
@@ -15,19 +14,14 @@ async def handle_messages(message: types.Message):
         await message.answer('Risposta al pulsante "Hello!"')
     elif message.text == 'Youtube':
         await message.answer('Risposta al pulsante "Youtube"')
-    elif message.text == 'prova':
-        with open('Elezioni_Villafranca.csv', 'r') as file:
-            reader = csv.reader(file)
-            first_row = next(reader)
-            lista = []
-            indice = [1,3,5];
-        for item in first_row:
-            lista.extend(item.split(';'))
-        listaSindaci = []
-        for i in indice:
-            listaSindaci.append(lista[i])
-            print(lista)
+
     await message.answer("yo", reply_markup=keyboard1)
 
+@dp.message_handler(commands=['read_csv'])
+async def read_csv_handler(message: types.Message):
+    df = pd.read_csv('file.csv', delimiter=';')
+    for index, row in df.iterrows():
+        values = ', '.join(row.astype(str))
+        await message.answer(values)
 
 executor.start_polling(dp)
